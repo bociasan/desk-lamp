755285cda73547a297b9c9d26c574ca97b1b9232
bociasan <bociasan95@gmail.com>
2023-01-19 01:08:13 +0200

    Added WiFiMulti

---
+++ include/credentials_model.h
@@ -0,0 +1,11 @@
+#ifndef CREDENTIALS_MODEL
+    #define CREDENTIALS
+    #define ANONYMUS_SSID 
+    #define ANONYMUS_PSWD 
+    #define LAMP_SSID
+    #define LAMP_PSWD
+    #define PROIECT_SCR_SSID 
+    #define PROIECT_SCR_PSWD 
+    #define BEAMLOGIC_SSID 
+    #define BEAMLOGIC_PSWD 
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 #include <StripBS.h>
 #include <ServerHeader.h>
+#include <credentials.h>
+#include <ESP8266WiFiMulti.h>
+
 
 #define pinStrip D1
 #define useSerial true
 // #define useSerial false
+
+ESP8266WiFiMulti wifiMulti;
+
+const char *ssid = LAMP_SSID;      // The name of the Wi-Fi network that will be created
+const char *password = LAMP_PSWD; // The password required to connect to it, leave blank for an open network
+
 
 #define k 0.02
 #define changingDelay 15
@@ -112,23 +121,43 @@ void initWebSocket() {
   server.addHandler(&ws);
 }
 
+void startWiFi()
+{                              // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+  WiFi.softAP(ssid, password); // Start the access point
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started\r\n");
 
+  wifiMulti.addAP(ANONYMUS_SSID, ANONYMUS_PSWD); // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP(BEAMLOGIC_SSID, BEAMLOGIC_PSWD);
+  wifiMulti.addAP(PROIECT_SCR_SSID, PROIECT_SCR_PSWD);
+  // wifiMulti.addAP("ssid_from_AP_3", "your_password_for_AP_3");
+
+  Serial.println("Connecting");
+  while (wifiMulti.run() != WL_CONNECTED && WiFi.softAPgetStationNum() < 1)
+  { // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  if (WiFi.softAPgetStationNum() == 0)
+  { // If the ESP is connected to an AP
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID()); // Tell us what network we're connected to
+    Serial.print("IP address:\t");
+    Serial.print(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
+  }
+  else
+  { // If a station is connected to the ESP SoftAP
+    Serial.print("Station connected to ESP8266 AP");
+  }
+  Serial.println("\r\n");
+}
 
 void setup() {
   Serial.begin(115200);
-  // WiFi.mode(WIFI_AP_STA);
-  WiFi.mode(WIFI_STA);
-  WiFi.hostname(device_name.c_str());
-  WiFi.enableInsecureWEP();
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println();
-  // Print ESP Local IP Address
-  Serial.println(WiFi.localIP());
+  
+  startWiFi();
 
   initWebSocket();
 
